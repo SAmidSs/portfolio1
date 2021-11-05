2c0d5148e429b0873ba0d2fec600d025003591a7
Dima Shkitin <amid2003s@gmail.com>
2021-11-05 20:39:02 +0200

    adaptive header

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,6 @@
 $(function () {
-  
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+        $('.menu__btn').toggleClass('menu__btn--active');
+    });
 })
